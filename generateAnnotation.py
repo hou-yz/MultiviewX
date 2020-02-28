stable calibration; bbox fix; unit conversion fix

generateAnnotation.py
@@ -27,8 +27,8 @@ def read_pom(fpath):
 
 def read_gt(cam):
     gt_3d = np.loadtxt(f'matchings/Camera{cam + 1}_3d.txt')
-    gt_3d = gt_3d[np.where(np.logical_and(gt_3d[:, -3] >= 0, gt_3d[:, -3] <= 25))[0], :]
-    gt_3d = gt_3d[np.where(np.logical_and(gt_3d[:, -2] >= 0, gt_3d[:, -2] <= 15))[0], :]
+    gt_3d = gt_3d[np.where(np.logical_and(gt_3d[:, -3] >= 0, gt_3d[:, -3] <= MAP_WIDTH))[0], :]
+    gt_3d = gt_3d[np.where(np.logical_and(gt_3d[:, -2] >= 0, gt_3d[:, -2] <= MAP_HEIGHT))[0], :]
     frame, pid = gt_3d[:, 0], gt_3d[:, 1]
     foot_3d_coord = gt_3d[:, -3:-1].transpose()
     pos = get_pos_from_worldcoord(foot_3d_coord)

unitConversion.py
@@ -17,7 +17,7 @@ def get_worldgrid_from_worldcoord(world_coord):
     coord_x, coord_y = world_coord
     grid_x = coord_x * MAP_EXPAND
     grid_y = coord_y * MAP_EXPAND
-    return np.array([grid_x, grid_y]).round()
+    return np.array([grid_x, grid_y], dtype=int)
 
 
 def get_worldcoord_from_worldgrid(worldgrid):